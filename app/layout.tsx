077a1b3f1dfdf0ff0c68885e90c7248acd2b1679
yurapa <yura.paray@gmail.com>
2025-02-25 09:56:46 +0200

    test GTM 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from 'next-themes';
-import { GoogleTagManager } from '@next/third-parties/google';
+// import { GoogleTagManager } from '@next/third-parties/google';
 
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from '@/lib/constants';
 import { Toaster } from '@/components/ui/toaster';
+import { GoogleAnalytics } from '@/components/analytics/google-analytics';
 
 import '@/assets/styles/globals.css';
 
@@ -26,20 +27,20 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const isGTM = !!process.env.NEXT_PUBLIC_GTM_ID;
+  // const isGTM = !!process.env.NEXT_PUBLIC_GTM_ID;
 
-  console.log('RootLayout -> isGTM', isGTM);
-  console.log('RootLayout -> process.env.NEXT_PUBLIC_GTM_ID;', process.env.NEXT_PUBLIC_GTM_ID);
+  // console.log('RootLayout -> isGTM', isGTM);
+  // console.log('RootLayout -> process.env.NEXT_PUBLIC_GTM_ID;', process.env.NEXT_PUBLIC_GTM_ID);
 
   return (
     <html lang="en" suppressHydrationWarning>
-      {isGTM && <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID as string} />}
       <body className={`${inter.className} antialiased`}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
           <Toaster />
         </ThemeProvider>
-        {process.env.NEXT_PUBLIC_GTM_ID}
+        <GoogleAnalytics />
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}
       </body>
     </html>
   );
