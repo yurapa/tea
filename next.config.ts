a6b597f10c37d6c19f5d0a1ffe21dccbb0ec0928
yurapa <yura.paray@gmail.com>
2025-09-15 00:10:18 +0300

    Update SEO files

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,114 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  // Add redirects to fix 404 errors from old/incorrect URLs
+  async redirects() {
+    return [
+      // Redirect old product slugs to actual existing products
+      {
+        source: '/product/earl-grey-classic',
+        destination: '/product/sir-charles-grey-tea',
+        permanent: true,
+      },
+      {
+        source: '/product/english-breakfast',
+        destination: '/product/english-aristocratic-tea',
+        permanent: true,
+      },
+      {
+        source: '/product/jasmine-green-tea',
+        destination: '/product/king-of-jasmine',
+        permanent: true,
+      },
+      {
+        source: '/product/chamomile-herbal',
+        destination: '/product/alpine-meadow',
+        permanent: true,
+      },
+      {
+        source: '/product/dragon-well-green',
+        destination: '/product/exclusive-gun-powder',
+        permanent: true,
+      },
+      {
+        source: '/product/pu-erh-aged',
+        destination: '/product/da_hong_pao_red_robe',
+        permanent: true,
+      },
+      {
+        source: '/product/white-silver-needle',
+        destination: '/product/white_tea_sun_chueng',
+        permanent: true,
+      },
+      {
+        source: '/product/oolong-iron-goddess',
+        destination: '/product/da_hong_pao_red_robe',
+        permanent: true,
+      },
+      {
+        source: '/product/rooibos-vanilla',
+        destination: '/product/carpathian-tea',
+        permanent: true,
+      },
+      {
+        source: '/product/tea-infuser-set',
+        destination: '/search?category=tea-accessories',
+        permanent: true,
+      },
+      // Add locale-specific redirects
+      {
+        source: '/:locale/product/earl-grey-classic',
+        destination: '/:locale/product/sir-charles-grey-tea',
+        permanent: true,
+      },
+      {
+        source: '/:locale/product/english-breakfast',
+        destination: '/:locale/product/english-aristocratic-tea',
+        permanent: true,
+      },
+      {
+        source: '/:locale/product/jasmine-green-tea',
+        destination: '/:locale/product/king-of-jasmine',
+        permanent: true,
+      },
+      {
+        source: '/:locale/product/chamomile-herbal',
+        destination: '/:locale/product/alpine-meadow',
+        permanent: true,
+      },
+      {
+        source: '/:locale/product/dragon-well-green',
+        destination: '/:locale/product/exclusive-gun-powder',
+        permanent: true,
+      },
+      {
+        source: '/:locale/product/pu-erh-aged',
+        destination: '/:locale/product/da_hong_pao_red_robe',
+        permanent: true,
+      },
+      {
+        source: '/:locale/product/white-silver-needle',
+        destination: '/:locale/product/white_tea_sun_chueng',
+        permanent: true,
+      },
+      {
+        source: '/:locale/product/oolong-iron-goddess',
+        destination: '/:locale/product/da_hong_pao_red_robe',
+        permanent: true,
+      },
+      {
+        source: '/:locale/product/rooibos-vanilla',
+        destination: '/:locale/product/carpathian-tea',
+        permanent: true,
+      },
+      {
+        source: '/:locale/product/tea-infuser-set',
+        destination: '/:locale/search?category=tea-accessories',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
