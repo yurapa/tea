2b70768d34d1e65185a0e8708886a638f58f483b
yurapa <yura.paray@gmail.com>
2025-09-20 14:55:45 +0300

    Unprotected NextJS, Prisma and other packages

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,4 +1,4 @@
-import { Pool, neonConfig } from '@neondatabase/serverless';
+import { neonConfig } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
 import ws from 'ws';
@@ -7,11 +7,8 @@ import ws from 'ws';
 neonConfig.webSocketConstructor = ws;
 const connectionString = `${process.env.DATABASE_URL}`;
 
-// Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
-const pool = new Pool({ connectionString });
-
-// Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
-const adapter = new PrismaNeon(pool);
+// Instantiates the Prisma adapter using the Neon connection configuration to handle the connection between Prisma and Neon.
+const adapter = new PrismaNeon({ connectionString });
 
 // Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
 export const prisma = new PrismaClient({ adapter }).$extends({
